47d499237395963a703380d790e8b748cdcdb6f1
LexiSuzuki <107337101+LexiSuzuki@users.noreply.github.com>
2022-10-11 22:32:45 -0400

    Add files via upload

---
+++ CemuStarter.sh
@@ -0,0 +1,37 @@
+#!/bin/sh
+clear
+PS3='-----Please chose an option-----'
+options=("Start" "Update" "Exit")
+select menu in "${options[@]}"; do
+    case $menu in
+        #Used for launching Cemu
+        "Start")
+            echo "Starting Cemu..."
+            echo "Finished!!"
+            echo "Following warnings are from Cemu, not this script"
+	    #Change this if your cemu folder is not on home
+	    cd ~/Cemu/bin
+	    ./Cemu_release
+            echo "----------Cemu closed----------"
+	    exit
+            ;;
+        #Used for Updating Cemu
+        "Update")
+            echo "Updateing..."
+        #please chang these paths according where you store Cemu
+	    cd ~/
+	    git clone --recursive https://github.com/cemu-project/Cemu
+	    cd Cemu
+	    cmake -S . -B build -DCMAKE_BUILD_TYPE=release -DCMAKE_C_COMPILER=/usr/bin/clang -DCMAKE_CXX_COMPILER=/usr/bin/clang++ -G Ninja
+	    cmake --build build
+            echo "Finished!!"
+        exit
+            ;;
+        #Exit the script
+        "Exit")
+            echo "Exiting..."
+        exit
+            ;;
+        *) echo "invalid option $REPLY";;
+    esac
+done
